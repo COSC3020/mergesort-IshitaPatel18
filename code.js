f713557ae0eccfd7dd00b521af50693cde799f98
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-02-21 17:20:23 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,39 @@
-function mergesort(array) {
-    return array;
+function mergesort(array)
+{
+  let size = array.length;
+  let tmp = Array(size).fill(0);
+
+  for(let i = 1; i <= size - 1; i *= 2 )
+    {
+      for(let leftS = 0; leftS < size - 1; leftS += 2 * i)
+        {
+          let mid = leftS + i - 1;
+          let rightE = Math.min(leftS + 2 * i - 1, size - 1);
+
+          tmp = merge(array, leftS, mid, rightE, tmp);
+        }
+    }
+  return array; 
+}
+
+function merge(array, leftS, mid, rightE, tmp)
+{
+  let a = leftS, b = mid + 1;
+  for(var k = leftS; k <= rightE; k++)
+  {
+    if(a <= mid && (b > rightE || array[a] < array[b]))
+    {
+      tmp[k] = array[a++];
+    }
+    else
+    {
+      tmp[k] = array[b++];
+    }
+  }
+
+  for(let k = leftS; k <= rightE; k++)
+  {
+    array[k] = tmp[k];  
+  }
+  return tmp;
 }
