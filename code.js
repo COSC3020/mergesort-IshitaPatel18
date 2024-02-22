aac67679d4740b5977a0d167a33b0c81d168a938
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-02-22 16:43:17 -0700

    maybe this is right

--- code.js
+++ code.js
@@ -1,42 +1,32 @@
-// Geeks for Geeks was used to understand the mechanics of the iteration of the for loops in mergesort
-// The merge code was taken from the slides
-
-function mergesort(array)
-{
+function mergesort(array) {
   let size = array.length;
-  let tmp = Array(size).fill(0);
 
-  for(let i = 1; i <= size - 1; i *= 2 )
-    {
-      for(let leftS = 0; leftS < size - 1; leftS += 2 * i)
-        {
-          let mid = leftS + i - 1;
-          let rightE = Math.min(leftS + 2 * i - 1, size - 1);
+  // Geeks for Geeks was used for this part of the code to solve the division iteration
+  for (let i = 1; i <= size - 1; i *= 2) {
+    for (let leftS = 0; leftS < size - 1; leftS += 2 * i) {
+      let mid = leftS + i - 1;
+      let rightE = Math.min(leftS + 2 * i - 1, size - 1);
 
-          tmp = merge(array, leftS, mid, rightE, tmp);
+      let first = leftS;
+      let second = mid + 1;
+      let tmp;
+      while (first <= mid && second <= rightE) {
+        if (array[first] <= array[second]) {
+          first++
         }
-    }
-  return array; 
-}
-
-function merge(array, leftS, mid, rightE, tmp)
-{
-  let a = leftS, b = mid + 1;
-  for(var k = leftS; k <= rightE; k++)
-  {
-    if(a <= mid && (b > rightE || array[a] < array[b]))
-    {
-      tmp[k] = array[a++];
-    }
-    else
-    {
-      tmp[k] = array[b++];
+        else {
+          //At this point I was stuck again and I got help from the mergesort done by Clayton Brown
+          tmp = array[second];
+          for (let shuffle = second; shuffle > first; shuffle--) {
+            array[shuffle] = array[shuffle - 1];
+          }
+          array[first] = tmp;
+          first++;
+          mid++;
+          second++;
+        }
+      }
     }
   }
-
-  for(let k = leftS; k <= rightE; k++)
-  {
-    array[k] = tmp[k];  
-  }
-  return tmp;
+  return array;
 }
